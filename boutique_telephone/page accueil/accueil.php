d007c2c1207a924ec9c37fe035b3c0ea0a05c5cd
KilianGravier <145116206+KilianGravier@users.noreply.github.com>
2023-09-25 16:29:05 +0200

    Add files via upload

---
+++ boutique_telephone/page accueil/accueil.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script></script>
+    <meta charset="UTF-8">
+    <title>Adidas</title>
+    <link rel="stylesheet" href="accueil.css">
+</head>
+<body>
+
+<?php include "../header/header.html";?>
+
+<main>
+    <section class="promotions">
+        <h2>Promotions</h2>
+        <div class="article">
+            <img id="img1" src="../images/xiaomi_mi_11_ultra.jpg" alt="article en promotion">
+            <h3>Xiaomi 11 Ultra </h3>
+            <p class="price">600 <del>700€</del></p>
+            <a href="#" class="buy">Acheter</a>
+        </div>
+        <div class="article">
+            <img id="img2" src="../images/iphone-13-pro.jfif" alt="article en promotion">
+            <h3>Iphone 13 Pro</h3>
+            <p class="price">800€ <del>1300€</del></p>
+            <a href="#" class="buy">Acheter</a>
+        </div>
+        <div class="article">
+            <img id="img3" src="../images/google.webp" alt="article en promotion">
+            <h3>Google Pixel 6 Pro</h3>
+            <p class="price">500€ <del>799€</del></p>
+            <a href="#" class="buy">Acheter</a>
+        </div>
+        <div class="article">
+            <img id="img4" src="../images/sony.avif" alt="article en promotion">
+            <h3>Sony Xperia 1 III</h3>
+            <p class="price">800€ <del>850€</del></p>
+            <a href="#" class="buy">Acheter</a>
+        </div>
+    </section>
+</main>
+
+	</body>
+    <footer>
+             <script src="script.js"></script> 
+              <?php include "../footer/footer.html";?>
+		
+	</footer>
+</html>
---
+++ boutique_telephone/page accueil/script.js
@@ -0,0 +1,25 @@
+// Récupérer les éléments HTML pertinents
+const articles = document.querySelectorAll('.promotions .article');
+const firstArticles = [articles[0], articles[1]];
+const lastArticles = [articles[2], articles[3]];
+
+// Cacher les deux derniers articles
+lastArticles.forEach(article => article.style.display = 'none');
+
+// Afficher les deux premiers articles pendant 5 secondes, puis les cacher et afficher les deux derniers
+setInterval(() => {
+  // Cacher les deux premiers articles
+  firstArticles.forEach(article => article.style.display = 'none');
+
+  // Afficher les deux derniers articles
+  lastArticles.forEach(article => article.style.display = 'block');
+
+  // Attendre 5 secondes
+  setTimeout(() => {
+    // Cacher les deux derniers articles
+    lastArticles.forEach(article => article.style.display = 'none');
+
+    // Afficher les deux premiers articles
+    firstArticles.forEach(article => article.style.display = 'block');
+  }, 5000);
+}, 10000);
