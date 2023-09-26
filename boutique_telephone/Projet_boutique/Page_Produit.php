4cc154079c8c5d285821ee0e42bc6ff388c365cf
Moninn <145116255+Moninn@users.noreply.github.com>
2023-09-26 09:20:30 +0200

    Update Page_Produit.php

--- boutique_telephone/Projet_boutique/Page_Produit.php
+++ boutique_telephone/Projet_boutique/Page_Produit.php
@@ -2,39 +2,28 @@
 // Connexion à la base de données
 $pdo = new PDO('mysql:host=localhost;dbname=boutique_tel', 'root', '');
 
-if (isset($_GET['id_produit'])) {
-    // Récupère la valeur de 'id_produit' depuis l'URL
-    $id_produit = $_GET['id_produit'];
-    
-    // Requête SQL pour sélectionner le produit avec l'ID correspondant
-    $query = "SELECT * FROM produit WHERE ID_PRODUIT = :id_produit";
-    $statement = $pdo->prepare($query);
-    $statement->bindParam(':id_produit', $id_produit);
-    $statement->execute();
+$triPrix = isset($_GET['triPrix']) ? $_GET['triPrix'] : 'croissant';
 
-    if ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $nom_produit = $row['NOM_PRODUIT'];
-        $description_produit = $row['DESCRIPTION_PRODUIT'];
-        $prix_produit = $row['PRIX_PRODUIT'];
-        $stock_produit = $row['STOCK_PRODUIT'];
-        $marque_tel = $row['MARQUE_TEL'];
-        $caracteristique_tel = $row['CARACTERISTIQUE_TEL'];
-        $url_image = $row['URL_IMAGE'];
-    } else {
-        header('Location: liste_produits.php');
-        exit();
-    }
+// Requête SQL pour récupérer les produits avec les filtres
+$query = "SELECT * FROM produit WHERE 1";
+
+// Appliquez le filtre de tri par prix
+if ($triPrix === 'croissant') {
+    $query .= " ORDER BY PRIX_PRODUIT ASC";
 } else {
-    header('Location: liste_produits.php');
-    exit();
+    $query .= " ORDER BY PRIX_PRODUIT DESC";
 }
+
+$statement = $pdo->prepare($query);
+$statement->execute();
 ?>
 
+<!DOCTYPE html>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title><?= $nom_produit ?></title>
+    <title>Nos Produits</title>
     <link href="Boutique.css" rel="stylesheet">
 </head>
 <body>
@@ -42,19 +31,55 @@ if (isset($_GET['id_produit'])) {
 <?php include 'header.html'; ?> <!-- Inclure le header -->
 
 <main>
-    <header>
-        <h2 class="h2_detail"><?= $nom_produit ?></h2>
-    </header>
-    <div class="detail_produit">
-        <h1 class="h1_detail"><?= $row['NOM_PRODUIT'] ?></h1>
-        <img class="image_detail" src="<?= $url_image ?>" alt="<?= $nom_produit ?>">
-        <div class="description_detail_produit">
-        <p><u>Description</u> : <?= $description_produit ?></p>
-        <p><u>Prix</u> : <?= $prix_produit ?> €</p>
-        <p><u>Stock</u> : <?= $stock_produit ?></p>
-        <p><u>Marque</u> : <?= $marque_tel ?></p>
-        <p><u>Caractéristique</u> : <?= $caracteristique_tel ?></p>
-        </div>
+    <form method="GET"> <!-- Ajout d'un formulaire pour les filtres -->
+        <header>
+            <label for="triPrix">Trier par prix :</label>
+            <select id="triPrix" name="triPrix"> 
+                <option value="croissant">Prix Croissant</option>
+                <option value="decroissant">Prix Décroissant</option>
+            </select>
+            <input type="submit" value="Appliquer">
+        </header>
+    </form> <!-- Fin du formulaire -->
+    <h1>Nos Produits</h1>
+    <div class="Produit">
+        <ul>
+            <?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
+                <li>
+                    <h2><?= $row['NOM_PRODUIT'] ?></h2>
+                    <!-- Lien vers la page de détail du produit avec l'ID du produit en paramètre -->
+                    <a href="detail_produit.php?id_produit=<?= $row['ID_PRODUIT'] ?>">
+                        <img class="image_produit" src="<?= $row['URL_IMAGE'] ?>" alt="Image du produit">
+                    </a>
+                    <div class="description_generale">
+                    <div class="description_produit">
+                    <p class="prix_produit"><u>Prix</u> : <?= $row['PRIX_PRODUIT'] ?> €</p>
+                    <p><u>Stock</u> : <?= $row['STOCK_PRODUIT'] ?></p>
+                    <p><u>Marque</u> : <?= $row['MARQUE_TEL'] ?></p>
+                    <p><u>Caractéristique</u> : <?= $row['CARACTERISTIQUE_TEL'] ?></p>
+                    </div>
+                    <div class="notation">
+                    <p><u>Note moyenne</u> :</p>
+                    </div>
+                    <?php
+                    // Génération d'une note aléatoire entre 1 et 5 étoiles
+                    $averageRating = rand(1, 5);
+
+                    // Affichez la note moyenne en tant que symboles d'étoiles
+                    echo '<p>';
+                    for ($i = 1; $i <= 5; $i++) {
+                        if ($i <= $averageRating) {
+                            echo '&#9733;'; // Étoile pleine
+                        } else {
+                            echo '&#9734;'; // Étoile vide
+                        }
+                    }
+                    echo '</p>';
+                    ?>
+                    </div>
+                </li>
+            <?php endwhile; ?>
+        </ul>
     </div>
 </main>
 
@@ -64,6 +89,7 @@ if (isset($_GET['id_produit'])) {
 </html>
 
 <?php
+// Fermeture de la connexion à la base de données
 $pdo = null;
 ?>
 
